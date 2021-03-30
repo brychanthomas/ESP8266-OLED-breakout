a23d150c37eb3b3b0b038aa3776d8b54333b4566
Brychan Thomas <brychan.thomas@btinternet.com>
2021-03-30 14:13:55 +0100

    Created random pixel program to test display

--- breakout/breakout.ino
+++ breakout/breakout.ino
@@ -1,10 +1,21 @@
-void setup() {
-  Serial.begin(9600);
+#include <Wire.h>
+#include "SSD1306Wire.h"
 
+SSD1306Wire display(0x3c, 5, 4);  // ADDRESS, SDA, SCL
+
+void setup() {
+  //Serial.begin(9600);
+  display.init();
+  display.setColor(WHITE);
 }
 
+int x;
+int y;
 void loop() {
-  Serial.println("Hello world!");
-  delay(2000);
+  x = random(1, 129);
+  y = random(1, 65);
+  display.setPixel(x, y);
+  delay(50);
+  display.display(); //write data in buffer to display
 
 }
