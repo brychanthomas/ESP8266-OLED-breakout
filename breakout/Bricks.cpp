4b421098cf1989e9ed6b7236f29aafb5800c75b8
Brychan Thomas <brychan.thomas@btinternet.com>
2021-04-05 00:38:25 +0100

    Added score counter to bricks class

--- breakout/Bricks.cpp
+++ breakout/Bricks.cpp
@@ -1,6 +1,7 @@
 #include "Bricks.h"
 
 Bricks::Bricks (SSD1306Wire* display){
+  score = 0;
   for (int col = 0; col < 4; col++) {
     for (int row = 0; row < 8; row++) {
       bricks[31-(row*4+col)] = Brick(row*8+1, col*16+1, 14, 6, display);
@@ -19,8 +20,13 @@ void Bricks::update(Ball* b) {
     if (bricks[i].exists) {
       if (b->checkBrickCollision(bricks[i].x, bricks[i].y, bricks[i].height, bricks[i].width)) {
         bricks[i].exists = false;
+        score += 1
         break;
       }
     }
   }
+}
+
+unsigned char Bricks::getScore() {
+  return score;
 }
--- breakout/Bricks.h
+++ breakout/Bricks.h
@@ -3,9 +3,11 @@
 class Bricks {
   private:
     Brick bricks[32];
+    unsigned char score;
 
   public:
     Bricks(SSD1306Wire*);
     void draw();
     void update(Ball*);
+    unsigned char getScore();
 };
